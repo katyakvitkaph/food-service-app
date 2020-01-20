8cf31154cf3ac8838107c03d082ece096bb0ba0c
katyakvitkaph <kvitka.katja@gmail.com>
2020-01-20 16:21:30 +0200

    last update

--- src/index.js
+++ src/index.js
@@ -1,75 +1,51 @@
-import './styles.css';
-import menu from './menu.json';
-import template from './template.hbs';
+import "./styles.css";
+import menu from "./menu.json";
+import template from "./template.hbs";
 
 const refs = {
-    menu: document.querySelector('.menu'),
-   template : document.querySelector('template'),
-   but : document.querySelector('#theme-switch-control'),
+  menu: document.querySelector(".menu"),
+  template: document.querySelector("template"),
+  but: document.querySelector("#theme-switch-control")
 };
 
+function makeHtml(menu) {
+  const markup = menu.map(item => template(item)).join("");
 
-
-
-function makeHtml (menu) {
-    const markup = menu.map(item => template(item)).join("");
-   
-    refs.menu.insertAdjacentHTML('beforeend', markup)
+  refs.menu.insertAdjacentHTML("beforeend", markup);
 }
 
 makeHtml(menu);
 
-
 // =========change theme
 
 const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
-const theme = 'theme';
+  LIGHT: "light-theme",
+  DARK: "dark-theme"
+};
+const theme = "theme";
 
 const changeColor = localStorage.getItem(theme);
-
-
- if (changeColor) {
-    refs.but.checked = changeColor === Theme.DARK
- }
-
-const themeInstalled = localStorage.getItem(theme);
-console.log(themeInstalled);
-if (themeInstalled) {
-    document.body.classList.add(localStorage.getItem(theme));
+if (changeColor) {
+  refs.but.checked = changeColor === Theme.DARK;
 }
 
+const themeInstalled = localStorage.getItem(theme);
+if (themeInstalled) {
+  document.body.classList.add(localStorage.getItem(theme));
+}
 
- const changeBack = e => {
-     if (!themeInstalled) {
-        document.body.classList.add(Theme.DARK );   
-     }
+const changeBack = e => {
+  if (!themeInstalled) {
+    document.body.classList.add(Theme.DARK);
+  }
 
-     console.log(e.target.checked)
-     if (e.target.checked){
-        console.log('dark');
-        localStorage.setItem(theme, Theme.DARK);
-
-        console.log('e.target.checked',e.target.checked);
-       
-        
-        // document.body.classList.remove(Theme.LIGHT );
-        document.body.classList.replace(Theme.LIGHT,Theme.DARK );
-    } else {
-        localStorage.setItem(theme, Theme.LIGHT);
-        // document.body.classList.remove(Theme.DARK);
-
-        document.body.classList.replace(Theme.DARK, Theme.LIGHT);
-        console.log('light');
-       
-      }
-    
+  if (e.target.checked) {
+    localStorage.setItem(theme, Theme.DARK);
+    document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+  } else {
+    localStorage.setItem(theme, Theme.LIGHT);
+    document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+  }
 };
 
-refs.but.addEventListener("change", changeBack)
-    
-
-
-
+refs.but.addEventListener("change", changeBack);
